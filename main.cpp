f52f126e967121dbb51f38d1b66820068272edd6
Mateusz Wieczorek <szachmat2004@gmail.com>
2024-03-16 16:12:06 +0100

    Adding preprocessing blocks for each operation, everything is almost working

--- main.cpp
+++ main.cpp
@@ -1,47 +1,89 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <deque>
+#include <vector>
+
+void removeDoubleWhitespaces(std::string& str) 
+{
+    for (size_t i = 0; i < str.size() - 1; ++i) 
+    {
+        if (std::isspace(str[i]) && std::isspace(str[i + 1])) 
+        {
+            // Erase the next whitespace
+            str.erase(i + 1, 1);
+            --i;
+        }
+    }
+}
+
+void replaceSingleWhitespacesWithDouble(std::string& str) 
+{
+    size_t pos = 0;
+    while ((pos = str.find(' ', pos)) != std::string::npos) 
+    {
+        str.replace(pos, 1, "  ");
+        pos += 2;
+    }
+}
 
 const int S_MAX = 100;
 
-int wage(char ch)
+int wage(std::string ch) 
 {
-    if (ch == '+' || ch == '-')
+    if (ch == "+" || ch == "-")
         return 1;
-    else if (ch == '*' || ch == '/')
+    else if (ch == "*" || ch == "/")
         return 2;
-    else if (ch == 'N' || ch == 'I' || ch == 'M' || ch == 'A') // I stand for "if" for now, "M" stands for min and 'A' stands for max
+    else if (ch == "N" || ch == "IF" || ch == "MIN" || ch == "MAX")
         return 3;
     return 0;
 }
 
-void operator_char(char Stack[S_MAX], int& index, char character, std::string& output)
-{
-    while (index > 0)
-    {
+void operator_char(std::string Stack[S_MAX], int& index, std::string& character, std::string& output) {
+    while (index > 0) {
         if (wage(character) == 3 || wage(character) > wage(Stack[index - 1]))
             break;
         output += Stack[--index];
         output += ' ';
     }
-    Stack[index++] = character;
+    if (index < S_MAX) // Check if index is within bounds before accessing the array
+        Stack[index++] = character;
 }
 
-int main()
+int main() 
 {
-    char Stack[S_MAX];
+    std::string Stack[S_MAX];
     int stack_index = 0;
     std::string output;
-    char character;
     int local_counter_stack[S_MAX];
     int local_stack_index = 0;
     bool first = true;
 
-    while (true)
+    std::string expression;
+    std::getline(std::cin, expression);
+    std::vector<std::string> separated_expression;
+    
+    std::string temp;
+    for (char c : expression) 
     {
-        std::cin >> character;
-        if (character == ',')
+        if (c != ' ')
+            temp += c;
+        else 
         {
-            while (stack_index > 0 && (Stack[stack_index - 1] == '+' || Stack[stack_index] == '-' || Stack[stack_index - 1] == '/' || Stack[stack_index] == '*'))
+            separated_expression.push_back(temp);
+            temp.clear();
+        }
+    }
+    if (temp != " ")
+        separated_expression.push_back(temp);
+
+    
+    for (auto string : separated_expression) 
+    {
+        if (string == ",") {
+            while (stack_index > 0 && (Stack[stack_index - 1] == "+" || Stack[stack_index - 1] == "-" 
+            || Stack[stack_index - 1] == "/" || Stack[stack_index - 1] == "*")) 
             {
                 stack_index--;
                 output += Stack[stack_index];
@@ -51,82 +93,184 @@ int main()
             continue;
         }
 
-        if (character == '.')
+        if (string == ".") 
         {
-            while (stack_index)
+            while (stack_index > 0) 
             {
                 output += Stack[--stack_index];
                 output += ' ';
             }
-            output += local_stack_index[local_counter_stack];
             break;
         }
 
-        switch (character)
-        {
-        case ' ': 
-            break;
-        case '(':
-            Stack[stack_index++] = character;
+        if (string == "(") {
+            Stack[stack_index++] = string;
             if (first)
-            {
                 first = false;
-            }
             else
-            {
                 local_stack_index++;
-            }
             local_counter_stack[local_stack_index] = 1;
-            break;
-        case ')':
-            /*
-            std::cout << "Stack here: \n";
-            for (int i = 0; i < local_stack_index; i++)
-                std::cout << local_counter_stack[i] << " ";
-            std::cout << std::endl;
-            */
-
-            while (stack_index > 0 && Stack[stack_index - 1] != '(')
+        }
+        else if (string == ")") 
+        {
+            while (stack_index > 0 && Stack[stack_index - 1] != "(") 
             {
                 output += Stack[--stack_index];
                 output += ' ';
             }
+
             if (stack_index > 0) 
                 stack_index--; // Remove '(' from the stack
 
-            if (Stack[stack_index - 1] == 'I' || Stack[stack_index - 1] == 'M' || Stack[stack_index - 1] == 'A')
+            if (stack_index > 0 && (Stack[stack_index - 1] == "IF" || Stack[stack_index - 1] == "MIN" || Stack[stack_index - 1] == "MAX")) 
             {
-                output += Stack[--stack_index];
-                if (Stack[stack_index - 1] != 'I')
+                output += Stack[stack_index - 1];
+                if (stack_index > 0 && Stack[stack_index - 1] != "IF")
                     output += '0' + local_counter_stack[local_stack_index];
+                stack_index--;
                 output += ' ';
             }
             local_counter_stack[local_stack_index] = 0;
             if (local_stack_index > 0)
                 local_stack_index--;
-            /*
-            std::cout << "Stack there: \n";
-            for (int i = 0; i < local_stack_index; i++)
-                std::cout << local_counter_stack[i] << " ";
-            std::cout << std::endl;
-            */
-            break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case 'N':
-        case 'I':
-        case 'M': // MIN
-        case 'A': // MAX
-            operator_char(Stack, stack_index, character, output);
-            break;  
-        default:
-            output += character;
+        }
+
+        else if (string == "+" || string == "-" || string == "/" || string == "*" || string == "MIN" || string == "MAX" || string == "IF" || string == "N") 
+            operator_char(Stack, stack_index, string, output);
+        else 
+        {
+            output += string;
             output += ' ';
-            break;
         }
     }
+    removeDoubleWhitespaces(output);
+    temp.clear();
+    separated_expression.clear();
+    for (char c : output)
+    {
+        if (c == ' ')
+        {
+            separated_expression.push_back(temp);
+            temp.clear();
+        }
+        else
+        {
+            temp += c;
+        }
+    }
+    replaceSingleWhitespacesWithDouble(output);
+    std::cout << output << std::endl;
+    
+    // Now when basic preprocessing is done we begin!
+    std::deque<std::string> queue;
+    
+    for (auto word : separated_expression)
+    {
+        if (word[0] == 'M' && word[1] == 'A')
+        {
+            int amount = (word[word.size() - 1]) - '0';
+            std::cout << word << " ";
+            int maxValue = -9999;
+            for (int i = 0; i < amount; i++)
+            {
+                int number = std::stoi(queue.back());
+                queue.pop_back();
+                maxValue = std::max(maxValue, number);
+                std::cout << number << " ";
+            }
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            queue.push_back(std::to_string(maxValue));
+        }
+        else if (word[0] == 'M' && word[1] == 'I')
+        {
+            int amount = (word[word.size() - 1]) - '0';
+            std::cout << word << " ";
+            int minValue = 9999;
+            for (int i = 0; i < amount; i++)
+            {
+                int number = std::stoi(queue.back());
+                queue.pop_back();
+                minValue = std::min(minValue, number);
+                std::cout << number << " ";
+            }
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            queue.push_back(std::to_string(minValue));
+        }
+        else if (word == "N")
+        {
+            int num1 = std::stoi(queue.back());
+            queue.pop_back();
+            std::cout << word << " " << num1 << " ";
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            num1 *= -1;
+            std::string result = std::to_string(num1);
+            queue.push_back(result);
+        }
+        else if (word == "IF")
+        {
+            std::cout << word << " ";
+            int first = std::stoi(queue.back());
+            queue.pop_back();
+            int second = std::stoi(queue.back());
+            queue.pop_back();
+            int third = std::stoi(queue.back());
+            queue.pop_back();
+            std::string result;
+            if (first > 0)
+                result = std::to_string(second);
+            else
+                result = std::to_string(third);
+            std::cout << first << " " << second << " " << third << " ";
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            queue.push_back(result);
+        }
+        else if (word == "*" || word == "/" || word == "+" || word == "-")
+        {
+            std::string second = queue.back();
+            queue.pop_back();
+            std::string first = queue.back();
+            queue.pop_back();
+            std::cout << word << " " << second << " " << first << " ";
 
-    std::cout << "Result:\n" << output << std::endl;
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            int num1 = std::stoi(first);
+            int num2 = std::stoi(second);
+            
+            std::string result;
+            if (word == "*")
+                result = std::to_string(num2 * num1);
+            else if (word == "/")
+                result = std::to_string(num2 / num1);
+            else if (word == "+")
+                result = std::to_string(num1 + num2);
+            else
+                result = std::to_string(num2 - num1);
+            queue.push_back(result);
+        }
+        else
+        {
+            queue.push_back(word);
+        }
+    }
+    std::cout << queue.back() << std::endl;
 }
--- project.cpp
+++ project.cpp
@@ -1,98 +1,122 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-int wage(char ch)
-{
-    if (ch == '+' || ch == '-')
-        return 1;
-    else if (ch == '*' || ch == '/')
-        return 2;
-    return 0;
-}
-
-void operator_char(std::vector<std::string>& Stack, int& index, char character, std::vector<std::string>& output)
-{
-    while (index > 0)
-    {
-        if (wage(character) >= wage(Stack[index - 1][0]))
-            break;
-        output.push_back(Stack[--index]);
-        Stack.pop_back();
-    }
-    if (character != ',')
-    {
-        Stack.push_back(std::string(1, character));
-        index++;
-    }
-}
+#include <queue>
+#include <algorithm>
 
 int main()
 {
-    //std::string expression = "MIN ( 100 , MAX ( 1 , 34 * 2 + 10 - 20 , 2 ) , 80 , 60 )";
-    std::string expression = "400 + ( 11 - ( 3 * 2 ) ) / 2 + 200";
-    std::vector<std::string> stack;
-    int index = 0;
-    std::string temp = "";
-    std::vector<std::string> output;
+    std::deque<std::string> queue;
+    std::vector<std::string> separated_expression = { "400", "N", "11", "3", "2", "*",  "-",  "2",  "/",  "+",  "200",  "N",  "N" , "+" };
 
-    for (char c : expression)
+    
+    for (auto word : separated_expression)
     {
-        if (c == ')')
+        if (word[0] == 'M' && word[1] == 'A')
         {
-            while (index > 0 && stack[index - 1] != "(")
-                output.push_back(stack[--index]);
-            if (index > 0) 
+            int amount = (word[word.size() - 1]) - '0';
+            std::cout << word << " ";
+            int maxValue = -9999;
+            for (int i = 0; i < amount; i++)
             {
-                index--;
-                stack.pop_back(); // remove '(' symbol
+                int number = std::stoi(queue.back());
+                queue.pop_back();
+                maxValue = std::max(maxValue, number);
+                std::cout << number << " ";
             }
-            if (stack[index - 1] == "MIN" || stack[index - 1] == "MAX" || stack[index - 1] == "N")
+            for (int i = queue.size() - 1; i >= 0; i--)
             {
-                output.push_back(stack[--index]);
-                stack.pop_back();
-            }            
-        }
-        else if (c == '(' || c == ' ')
-        {
-            if (!temp.empty())
-            {
-                if (temp[0] == '1' || temp[0] == '2' || temp[0] == '3' || temp[0] == '4' 
-                || temp[0] == '5' || temp[0] == '6' || temp[0] == '7' || temp[0] == '8' || temp[0] == '9')
-                {
-                    output.push_back(temp);
-                }
-                else
-                {
-                    stack.push_back(temp);
-                    index++;
-                }
-                temp.clear();
+                std::cout << queue[i] << " ";
             }
+            std::cout << std::endl;
+            queue.push_back(std::to_string(maxValue));
         }
-        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == ',')
+        else if (word[0] == 'M' && word[1] == 'I')
         {
-            operator_char(stack, index, c, output);
+            int amount = (word[word.size() - 1]) - '0';
+            std::cout << word << " ";
+            int minValue = 9999;
+            for (int i = 0; i < amount; i++)
+            {
+                int number = std::stoi(queue.back());
+                queue.pop_back();
+                minValue = std::min(minValue, number);
+                std::cout << number << " ";
+            }
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            queue.push_back(std::to_string(minValue));
+        }
+        else if (word == "N")
+        {
+            int num1 = std::stoi(queue.back());
+            queue.pop_back();
+            std::cout << word << " " << num1 << " ";
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            num1 *= -1;
+            std::string result = std::to_string(num1);
+            queue.push_back(result);
+        }
+        else if (word == "IF")
+        {
+            std::cout << word << " ";
+            int first = std::stoi(queue.back());
+            queue.pop_back();
+            int second = std::stoi(queue.back());
+            queue.pop_back();
+            int third = std::stoi(queue.back());
+            queue.pop_back();
+            std::string result;
+            if (first > 0)
+                result = std::to_string(second);
+            else
+                result = std::to_string(third);
+            std::cout << first << " " << second << " " << third << " ";
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            queue.push_back(result);
+        }
+        else if (word == "*" || word == "/" || word == "+" || word == "-")
+        {
+            std::string second = queue.back();
+            queue.pop_back();
+            std::string first = queue.back();
+            queue.pop_back();
+            std::cout << word << " " << second << " " << first << " ";
+
+            for (int i = queue.size() - 1; i >= 0; i--)
+            {
+                std::cout << queue[i] << " ";
+            }
+            std::cout << std::endl;
+            int num1 = std::stoi(first);
+            int num2 = std::stoi(second);
+            
+            std::string result;
+            if (word == "*")
+                result = std::to_string(num2 * num1);
+            else if (word == "/")
+                result = std::to_string(num2 / num1);
+            else if (word == "+")
+                result = std::to_string(num1 + num2);
+            else
+                result = std::to_string(num2 - num1);
+            queue.push_back(result);
         }
         else
         {
-            temp += c;
+            queue.push_back(word);
         }
     }
-
-    // Output the separated pieces
-    for (const std::string& piece : stack)
-    {
-        std::cout << piece << std::endl;
-    }
-    for (int i = index; i < stack.size(); i++)
-        output.push_back(stack[i]);
-
-    std::cout << "Result:\n";
-    for (const std::string& piece : output)
-    {
-        std::cout << piece << std::endl;
-    }
-
-    return 0;
+    std::cout << queue.back() << std::endl;
 }
